8cd77dbd1af14af139106daa8cc312dc0ce6c615
art241111 <24112000a>
2020-04-07 12:56:59 +0300

    Creating an array is put in a separate method

---
+++ app/src/main/java/ru/art241111/dish_recipes/ui/mainActivity/createArray/addItemsToArray.java
@@ -0,0 +1,18 @@
+package ru.art241111.dish_recipes.ui.mainActivity.createArray;
+
+import java.util.ArrayList;
+
+import ru.art241111.dish_recipes.R;
+import ru.art241111.dish_recipes.models.Dish;
+
+import static ru.art241111.dish_recipes.utils.constantSting.*;
+
+
+public class addItemsToArray {
+    public static void addItemsToArrayList(ArrayList<Dish> dishArrayList) {
+        dishArrayList.add(new Dish(R.drawable.marghuerita,PIZZA_MARGHUERITA_NAME,PIZZA_MARGHUERITA_DESCRIPTION, PIZZA_MARGHUERITA_RECIPE));
+        dishArrayList.add(new Dish(R.drawable.margherita_in_four, PIZZA_MARGHUERITA_4_NAME,PIZZA_MARGHUERITA_4_DESCRIPTION,PIZZA_MARGHUERITA_4_RECIPE));
+        dishArrayList.add(new Dish(R.drawable.ceramelised, PIZZA_CERAMELISED_NAME,PIZZA_CERAMELISED_DESCRIPTION,PIZZA_CERAMELISED_RECIPE));
+        dishArrayList.add(new Dish(R.drawable.superhealthy, PIZZA_SUPERHEALTHY_NAME,PIZZA_SUPERHEALTHY_DESCRIPTION,PIZZA_SUPERHEALTHY_RECIPE));
+    }
+}
