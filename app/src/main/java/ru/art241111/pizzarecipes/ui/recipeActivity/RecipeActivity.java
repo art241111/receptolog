67b99c53993eb33a2897739f4f3368be5c81a5dc
art241111 <24112000a>
2020-04-07 08:53:50 +0300

    Refactoring RecipeActivity

--- app/src/main/java/ru/art241111/pizzarecipes/ui/recipeActivity/RecipeActivity.java
+++ app/src/main/java/ru/art241111/pizzarecipes/ui/recipeActivity/RecipeActivity.java
@@ -2,6 +2,7 @@ package ru.art241111.pizzarecipes.ui.recipeActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import ru.art241111.pizzarecipes.R;
+import ru.art241111.pizzarecipes.models.Dish;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,20 +10,35 @@ import android.widget.TextView;
 
 public class RecipeActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
+        Dish dish = new Dish();
+
+        getDataFromIntent(dish);
+
+        setTextToTextView(dish);
+    }
+
+    private void setTextToTextView(Dish dish) {
         TextView name = findViewById(R.id.tv_name_dish);
         TextView description = findViewById(R.id.tv_description_dish);
         TextView recipe = findViewById(R.id.tv_recipe_dish);
 
+        name.setText(dish.getNameDish());
+        description.setText(dish.getDescriptionDish());
+        recipe.setText(dish.getRecipe());
+    }
+
+    private void getDataFromIntent(Dish dish) {
         Intent intent = getIntent();
         if(intent != null){
-            name.setText(intent.getStringExtra("name"));
-            description.setText(intent.getStringExtra("description"));
-            recipe.setText(intent.getStringExtra("recipe"));
+            dish.setNameDish(intent.getStringExtra("name"));
+            dish.setDescriptionDish(intent.getStringExtra("description"));
+            dish.setRecipe(intent.getStringExtra("recipe"));
         }
     }
 }
