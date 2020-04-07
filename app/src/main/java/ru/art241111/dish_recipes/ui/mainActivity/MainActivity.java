13c3345dbb9706e59788daaec09bc89e1838a878
art241111 <24112000a>
2020-04-07 10:54:21 +0300

    Add reaction on click

--- app/src/main/java/ru/art241111/dish_recipes/ui/mainActivity/MainActivity.java
+++ app/src/main/java/ru/art241111/dish_recipes/ui/mainActivity/MainActivity.java
@@ -8,6 +8,11 @@ import ru.art241111.dish_recipes.R;
 import ru.art241111.dish_recipes.adapters.RecyclerViewAdapter;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -19,10 +24,21 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
 
+    EditText et_ingredients;
+
+    ArrayList<String> ingredients;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ingredients = new ArrayList<>();
+
+        et_ingredients = findViewById(R.id.et_ingredients);
+        setListenerOnET(et_ingredients);
+
+        setListenerOnButton((Button) findViewById(R.id.bt_add_ingredients));
 
         ArrayList<Dish> dishArrayList = new ArrayList<>();
         addItemsToArrayList(dishArrayList);
@@ -30,6 +46,35 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.rv_dish);
         customizationRecyclerView(dishArrayList);
 
+    }
+
+    private void addIngredients(){
+        ingredients.add(et_ingredients.getText().toString());
+    }
+
+    private void setListenerOnButton(Button bt_add_ingredients) {
+        bt_add_ingredients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addIngredients();
+            }
+        });
+    }
+
+    private void setListenerOnET(final EditText editText) {
+        editText.setOnKeyListener(new View.OnKeyListener()  {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                            (keyCode == KeyEvent.KEYCODE_ENTER))
+                    {
+                        // сохраняем текст, введенный до нажатия Enter в переменную
+                        addIngredients();
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        );
     }
 
     private void customizationRecyclerView(ArrayList<Dish> dishArrayList) {
