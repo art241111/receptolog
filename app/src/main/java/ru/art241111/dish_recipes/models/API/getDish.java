55eada2038e45a026f582d039aa8740f3a44491b
art241111 <24112000a>
2020-05-01 18:30:31 +0300

    Refactoring create array class

--- app/src/main/java/ru/art241111/dish_recipes/models/API/getDish.java
+++ app/src/main/java/ru/art241111/dish_recipes/models/API/getDish.java
@@ -1,4 +1,4 @@
-package ru.art241111.dish_recipes.models.createArray;
+package ru.art241111.dish_recipes.models.API;
 
 import java.util.ArrayList;
 
@@ -12,11 +12,13 @@ import static ru.art241111.dish_recipes.utils.createDish.*;
  * Тоже времеенный класс, который добавляет в массив значения рецептов
  * В будующем бдет заменен на подгрузку данных из БД
  */
-public class addItemsToArray {
-    public static void addItemsToArrayList(ArrayList<Dish> fullDishArrayList) {
+public class getDish {
+    public static ArrayList<Dish> addItemsToArrayList() {
+        ArrayList<Dish> fullDishArrayList = new ArrayList<>();
         fullDishArrayList.add(new FullDish(R.drawable.marghuerita,PIZZA_MARGHUERITA_NAME,PIZZA_MARGHUERITA_DESCRIPTION, PIZZA_MARGHUERITA_RECIPE));
         fullDishArrayList.add(new FullDish(R.drawable.margherita_in_four, PIZZA_MARGHUERITA_4_NAME,PIZZA_MARGHUERITA_4_DESCRIPTION,PIZZA_MARGHUERITA_4_RECIPE));
         fullDishArrayList.add(new FullDish(R.drawable.ceramelised, PIZZA_CERAMELISED_NAME,PIZZA_CERAMELISED_DESCRIPTION,PIZZA_CERAMELISED_RECIPE));
         fullDishArrayList.add(new FullDish(R.drawable.superhealthy, PIZZA_SUPERHEALTHY_NAME,PIZZA_SUPERHEALTHY_DESCRIPTION,PIZZA_SUPERHEALTHY_RECIPE));
+        return fullDishArrayList;
     }
 }
