86ca79b96635d38b0625c758f9d2b2ff85b866b4
art241111 <24112000a>
2020-05-01 18:41:39 +0300

    Add code to remote repository

--- app/src/main/java/ru/art241111/dish_recipes/models/remoteDataSource/DishRemoteDataSource.kt
+++ app/src/main/java/ru/art241111/dish_recipes/models/remoteDataSource/DishRemoteDataSource.kt
@@ -1,4 +1,11 @@
 package ru.art241111.dish_recipes.models.remoteDataSource
 
+import android.os.Handler
+import ru.art241111.dish_recipes.models.API.getDishes.getDishes
+
 class DishRemoteDataSource {
+    fun getRepositories(onRepositoryReadyCallback: OnDishRemoteReadyCallback) {
+        val arrayList = getDishes()
+        Handler().postDelayed({ onRepositoryReadyCallback.onRemoteDataReady(arrayList) }, 2000)
+    }
 }
--- app/src/main/java/ru/art241111/dish_recipes/models/remoteDataSource/OnDishRemoteReadyCallback.kt
+++ app/src/main/java/ru/art241111/dish_recipes/models/remoteDataSource/OnDishRemoteReadyCallback.kt
@@ -1,4 +1,7 @@
 package ru.art241111.dish_recipes.models.remoteDataSource
 
+import ru.art241111.dish_recipes.data.Dish
+
 interface OnDishRemoteReadyCallback {
+    fun onRemoteDataReady(data: ArrayList<Dish>)
 }
