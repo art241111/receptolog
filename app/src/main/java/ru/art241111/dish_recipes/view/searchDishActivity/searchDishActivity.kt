ea57a9a1e21503831a4c6fc4c8ffdb1a7f64e36c
art241111 <24112000a>
2020-05-07 15:25:00 +0300

    Refactoring searchDishActivity

--- app/src/main/java/ru/art241111/dish_recipes/view/searchDishActivity/searchDishActivity.kt
+++ app/src/main/java/ru/art241111/dish_recipes/view/searchDishActivity/searchDishActivity.kt
@@ -1,5 +1,6 @@
 package ru.art241111.dish_recipes.view.searchDishActivity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -9,35 +10,52 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import ru.art241111.dish_recipes.R
 import ru.art241111.dish_recipes.adapters.DishesRecyclerViewAdapter
 import ru.art241111.dish_recipes.databinding.ActivitySearchDishBinding
+import ru.art241111.dish_recipes.view.recipeActivity.RecipeActivity
 import ru.art241111.dish_recipes.view_models.SearchDishViewModel
 
 class SearchDishActivity : AppCompatActivity(), DishesRecyclerViewAdapter.OnItemClickListener {
-
-    private lateinit var binding:ActivitySearchDishBinding
-    private val dishesRecyclerViewAdapter = DishesRecyclerViewAdapter(arrayListOf(), this)
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this,
+        // A binding with layout
+        val binding:ActivitySearchDishBinding = DataBindingUtil.setContentView(this,
                 R.layout.activity_search_dish
         )
+
+        // Add viewModel to binding
         val viewModel = ViewModelProviders.of(this).get(SearchDishViewModel::class.java)
         binding.viewModel = viewModel
         binding.executePendingBindings()
 
+        // Customization RecycleView: set layoutManager, adapter, data.
+        customizationRecycleView(binding, viewModel)
+    }
+
+    /**
+     * customization RecycleView: set layoutManager, adapter, data.
+     * @param binding - link to the layout,
+     * @param viewModel - link to the viewModel.
+     */
+    private fun customizationRecycleView(binding:ActivitySearchDishBinding, viewModel: SearchDishViewModel) {
+        val dishesRecyclerViewAdapter = DishesRecyclerViewAdapter(arrayListOf(), this)
         binding.rvDish.layoutManager = LinearLayoutManager(this)
         binding.rvDish.adapter = dishesRecyclerViewAdapter
-        viewModel.repositories.observe(this,
+        viewModel.dishes.observe(this,
                 Observer{ it?.let{ dishesRecyclerViewAdapter.replaceData(it)} })
 
         viewModel.loadDishes()
-
     }
 
+    /**
+     * this method works when the user clicks on an element RecycleView.
+     * @param position - the position of the item on which the user clicked.
+     */
     override fun onItemClick(position: Int) {
-        TODO("Not yet implemented")
+        val viewModel = ViewModelProviders.of(this).get(SearchDishViewModel::class.java)
+        val intent = Intent(this, RecipeActivity::class.java)
+        intent.putExtra("Dish", viewModel.dishes.value?.get(position))
+
+        this.startActivity(intent)
     }
 
 
