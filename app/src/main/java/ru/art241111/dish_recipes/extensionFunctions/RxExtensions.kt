ff66112150295bdd5bcaaecb8a91e528f7e796cb
art241111 <24112000a>
2020-05-07 13:53:07 +0300

    Migrate viewModel to RxJava2

---
+++ app/src/main/java/ru/art241111/dish_recipes/extensionFunctions/RxExtensions.kt
@@ -0,0 +1,8 @@
+package ru.art241111.kotlinmvvm.extensionFunctions
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
+    add(disposable)
+}
--- app/src/main/java/ru/art241111/dish_recipes/view_models/SearchDishViewModel.kt
+++ app/src/main/java/ru/art241111/dish_recipes/view_models/SearchDishViewModel.kt
@@ -4,9 +4,14 @@ import android.app.Application
 import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
 import ru.art241111.dish_recipes.data.FullDish
 import ru.art241111.dish_recipes.managers.NetManager
 import ru.art241111.dish_recipes.models.DishRepository
+import ru.art241111.kotlinmvvm.extensionFunctions.plusAssign
 
 
 class SearchDishViewModel(application: Application)
@@ -17,13 +22,36 @@ class SearchDishViewModel(application: Application)
 
     val isLoading = ObservableField(false)
 
+    // TODO: Read about Disposable
+    private val compositeDisposable = CompositeDisposable()
+
     fun loadDishes() {
-        dishRepository.getRepositories(object : OnRepositoryReadyCallback {
-            override fun onDataReady(data: ArrayList<FullDish>) {
-                isLoading.set(false)
-                repositories.value = data
-            }
-        })
+        isLoading.set(true)
+        compositeDisposable += dishRepository
+                .getRepositories()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableObserver<ArrayList<FullDish>>() {
+                    override fun onError(e: Throwable) {
+                        //if some error happens in our data layer our app will not crash, we will
+                        // get error here
+                    }
+
+                    override fun onNext(data: ArrayList<FullDish>) {
+                        repositories.value = data
+                    }
+
+                    override fun onComplete() {
+                        isLoading.set(false)
+                    }
+                })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        if (!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
     }
 
 }
