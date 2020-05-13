c13ee74b789b1c8ed6826fe68fb42589bbc254b5
art241111 <art241111@mail.ru>
2020-05-13 18:56:21 +0300

    Cleaning project

--- app/src/main/java/ru/art241111/dish_recipes/view/navigation/CreateInstance.kt
+++
@@ -1,7 +0,0 @@
-package ru.art241111.dish_recipes.view.navigation
-
-import ru.art241111.dish_recipes.view.viewDishActivity.ViewDishActivity
-
-interface CreateInstance {
-    fun createInstance(newInstance: ViewDishActivity)
-}
--- app/src/main/java/ru/art241111/dish_recipes/view/navigation/OpenFragment.kt
+++
@@ -1,5 +0,0 @@
-package ru.art241111.dish_recipes.view.navigation
-
-interface OpenFragment {
-    fun openNewFragment()
-}
